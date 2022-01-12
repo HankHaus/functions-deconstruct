0257b01c09f82e8006a91497774e11576e54770d
Henry House <hankhaus99@gmail.com>
2022-01-12 10:47:55 -0600

    solved cube

---
+++ main.js
@@ -0,0 +1,11 @@
+const whiteCross = () => {
+console.log(`make the white cross`)
+};
+
+const corners = () => {
+    console.log(`place the first layer corner pieces`)
+};
+
+const secondAndThirdLayers = () => {
+    console.log(`solve second and third layers`)
+};
